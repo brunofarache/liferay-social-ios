63ee07ec82fd565d703ae143bd5e48de5eb009fd
brunofarache <bruno.farache@liferay.com>
2013-05-15 19:06:01 -0300

    Initial Commit

---
+++ Liferay Social/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Liferay Social
+//
+//  Created by Bruno Farache on 15/05/13.
+//  Copyright (c) 2013 Bruno Farache. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ Liferay Social/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Liferay Social
+//
+//  Created by Bruno Farache on 15/05/13.
+//  Copyright (c) 2013 Bruno Farache. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
