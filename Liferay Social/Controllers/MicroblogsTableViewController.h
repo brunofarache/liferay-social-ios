c4d481429b1c0b36835b0b1abceb7e8611a476ad
Silvio Santos <silvio.santos@liferay.com>
2014-01-02 12:20:24 -0300

    Show username and content for each entry

--- Liferay Social/Controllers/MicroblogsTableViewController.h
+++ Liferay Social/Controllers/MicroblogsTableViewController.h
@@ -7,6 +7,10 @@
 
 #import <UIKit/UIKit.h>
 
+#define CELL_PADDING 26
+#define DETAIL_FONT_SIZE 12
+#define TITLE_FONT_SIZE 14
+
 @interface MicroblogsTableViewController : UITableViewController
 
 @property (nonatomic, strong) NSMutableArray *entries;
--- Liferay Social/Controllers/MicroblogsTableViewController.m
+++ Liferay Social/Controllers/MicroblogsTableViewController.m
@@ -12,23 +12,63 @@
 @implementation MicroblogsTableViewController
 
 - (id)init {
-    self = [super initWithStyle:UITableViewStylePlain];
+	self = [super initWithStyle:UITableViewStylePlain];
 
-    if (self) {
+	if (self) {
 		self.title = @"Microblogs";
 		self.tabBarItem.image = [UIImage imageNamed:@"first"];
 
 		[MicroblogsService getMicroblogsEntries:self
 			loadingView:self.navigationController.view];
-    }
+	}
 
-    return self;
+	return self;
 }
 
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+	return 1;
+}
+
+- (void)tableView:(UITableView *)tableView
+		didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView
+		heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+	MicroblogsEntryModel *entry = [self.entries objectAtIndex:indexPath.row];
+
+	NSString *username = [entry userName];
+	NSString *content = [entry content];
+
+	CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+
+	UIFont *titleFont = [UIFont boldSystemFontOfSize:TITLE_FONT_SIZE];
+	UIFont *detailFont = [UIFont systemFontOfSize:DETAIL_FONT_SIZE];
+
+	NSDictionary *titleAtributtes =
+		[NSDictionary dictionaryWithObject:titleFont
+		forKey: NSFontAttributeName];
+
+	CGSize titleSize =
+		[username boundingRectWithSize:screenSize
+		options:NSStringDrawingUsesLineFragmentOrigin
+		attributes:titleAtributtes context:nil].size;
+
+	NSDictionary *detailAtributtes =
+		[NSDictionary dictionaryWithObject:detailFont
+		forKey: NSFontAttributeName];
+
+	CGSize detailSize =
+		[content boundingRectWithSize:screenSize
+		options:NSStringDrawingUsesLineFragmentOrigin
+		attributes:detailAtributtes context:nil].size;
+
+	return (titleSize.height + detailSize.height + CELL_PADDING);
 }
 
 - (NSInteger)tableView:(UITableView *)tableView
@@ -48,15 +88,20 @@
 	if (cell == nil) {
 		cell =
 			[[UITableViewCell alloc]
-				initWithStyle:UITableViewCellStyleDefault
+				initWithStyle:UITableViewCellStyleSubtitle
 				reuseIdentifier:CellIdentifier];
-    }
+	}
 
 	MicroblogsEntryModel *entry = [self.entries objectAtIndex:indexPath.row];
 
-	cell.textLabel.text = entry.content;
+	cell.textLabel.font = [UIFont boldSystemFontOfSize:TITLE_FONT_SIZE];
+	cell.textLabel.text = entry.userName;
 
-    return cell;
+	cell.detailTextLabel.font = [UIFont systemFontOfSize:DETAIL_FONT_SIZE];
+	cell.detailTextLabel.text = entry.content;
+	cell.detailTextLabel.numberOfLines = 0;
+
+	return cell;
 }
 
 @end
