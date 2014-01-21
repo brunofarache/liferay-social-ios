fd9925b4a7e2c372f88b2358e52ede3a55856917
josianebezerra <josiane.bezerra@liferay.com>
2014-01-21 11:04:58 -0300

    Source formatting

--- Liferay Social/Delegates/LiferaySocialAppDelegate.h
+++ Liferay Social/Delegates/LiferaySocialAppDelegate.h
@@ -10,7 +10,7 @@
 @interface LiferaySocialAppDelegate : UIResponder
 	<UIApplicationDelegate, UITabBarControllerDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) UITabBarController *tabBarController;
+@property (strong, nonatomic) UIWindow *window;
 
 @end
