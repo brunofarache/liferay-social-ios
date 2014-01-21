9e756623473b47d332301cca5c08f291d38c8f84
josianebezerra <josiane.bezerra@liferay.com>
2014-01-21 11:58:42 -0300

    Source formatting

--- Liferay Social/Supporting Files/main.m
+++ Liferay Social/Supporting Files/main.m
@@ -5,17 +5,8 @@
 //	Bruno Farache
 //
 
-#import <UIKit/UIKit.h>
-
-#import "LiferaySocialAppDelegate.h"
-
 int main(int argc, char *argv[]) {
-
 	@autoreleasepool {
-		NSString *className = NSStringFromClass(
-			[LiferaySocialAppDelegate class]);
-		
-	    return UIApplicationMain(argc, argv, nil, className);
+		return UIApplicationMain(argc, argv, nil, @"LiferaySocialAppDelegate");
 	}
-
 }
