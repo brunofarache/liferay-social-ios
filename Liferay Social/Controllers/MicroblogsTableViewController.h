d241e9dde087c91cd6c8d55097ce86ae1fb10eaa
josianebezerra <josiane.bezerra@liferay.com>
2014-01-23 18:47:34 -0300

    Source formatting

--- Liferay Social/Controllers/MicroblogsTableViewController.h
+++ Liferay Social/Controllers/MicroblogsTableViewController.h
@@ -7,9 +7,9 @@
 
 #import "MicroblogsEntryModel.h"
 
-#define CELL_PADDING 26
-#define DETAIL_FONT [UIFont systemFontOfSize:12]
-#define TITLE_FONT [UIFont boldSystemFontOfSize:14]
+#define CELL_PADDING	26
+#define DETAIL_FONT		[UIFont systemFontOfSize:12]
+#define TITLE_FONT		[UIFont boldSystemFontOfSize:14]
 
 @interface MicroblogsTableViewController : UITableViewController
 
